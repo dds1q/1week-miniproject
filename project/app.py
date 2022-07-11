e23f07918da2a67fd932d36c2e908becba6da25c
BackDev19 <ghkdrnjs180@naver.com>
2022-07-11 22:41:21 +0900

    이미지 추가기능 추가

--- project/app.py
+++ project/app.py
@@ -58,5 +58,24 @@ def admin(name):
 def get_message(user_id):
    return 'Your ID is %d' % user_id
 
+@app.route("/submit", methods=["POST"])
+def web_write_post():
+    title_receive = request.form['title_give']
+    img_receive = request.form['img_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'title' : title_receive,
+        'img' : img_receive,
+        'comment' : comment_receive
+    }
+    db.App.insert_one(doc)
+
+    return jsonify({'msg': '등록완료'})
+
+@app.route("/submit", methods=["GET"])
+def web_write_get():
+    write_list = list(db.App.find({}, {'_id': False}))
+    return jsonify({'orders': write_list})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
