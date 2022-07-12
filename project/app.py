887b239dd563712f92032146aecbe671f089c110
BackDev19 <ghkdrnjs180@naver.com>
2022-07-12 12:42:47 +0900

    폰트, bulda 추가

--- project/app.py
+++ project/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
+from datetime import datetime
 import os
 app = Flask(__name__)
-
 # import requests
 # from bs4 import BeautifulSoup
 
@@ -13,42 +13,6 @@ db = client.dbsparta
 @app.route('/')
 def home():
     return render_template('index.html')
-#
-# @app.route("/travel", methods=["POST"])
-# def movie_post():
-#     url_receive = request.form['url_give']
-#     star_receive = request.form['star_give']
-#     comment_receive = request.form['comment_give']
-#
-#     filename = len(list(db.travel.find({},{'_id':False}))) + 1
-#     doc = {
-#         'url' : url_receive,
-#         'star' : star_receive,
-#         'comment' : comment_receive,
-#         'filename' : filename
-#     }
-#
-#     db.travel.insert_one(doc)
-#     return jsonify({'msg':'저장 완료!'})
-#
-# #파일 업로드 처리
-# @app.route('/fileUpload', methods = ['GET', 'POST'])
-# def upload_file():
-#    if request.method == 'POST':
-#       f = request.files['file']
-#       filename = len(list(db.travel.find({}, {'_id': False}))) + 1
-#
-#       #저장할 경로 + 파일명
-#       f.save("./static/images/" + str(filename) + ".png")
-#       # f.save("./static/1.png")
-#       return 'uploads 디렉토리 -> 파일 업로드 성공!'
-#       # return render_template('fileUpload.html')
-#
-#
-# @app.route("/travel", methods=["GET"])
-# def movie_get():
-#     travel_list = list(db.travel.find({}, {'_id' : False}))
-#     return jsonify({'travel': travel_list})
 
 @app.route("/about/<name>")
 def admin(name):
@@ -65,13 +29,15 @@ def web_write_post():
     comment_receive = request.form['comment_give']
     star_receive = request.form['star_give']
     desc_receive = request.form['desc_give']
+    mytime = datetime.now().strftime('%Y-%m-%d %H:%M')
 
     doc = {
         'title' : title_receive,
         'img' : img_receive,
         'comment' : comment_receive,
         'star' : star_receive,
-        'desc' : desc_receive
+        'desc' : desc_receive,
+        'time' : mytime
     }
     db.App.insert_one(doc)
 
